847f36b2e7f7238dcc1235a1da4082f778bb17fc
James Woods <33423469+jimbob1141@users.noreply.github.com>
2019-04-03 17:29:05 +0100

    Fixed incorrect indent

--- bitcoin_track.py
+++ bitcoin_track.py
@@ -14,21 +14,21 @@ def get_data():
     new_data = ''
     old_data = ''
     #Loops continuously
-    while True
+    while True:
       #gets current btc information
       btc = requests.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
       #converts the string response to json
       new_data = json.loads(btc.text)
         #compares new data to the previous data returned, if the data has changed it appends
         #the new data to the lists
-        if new_data != old_data:
-          btc_time.append(new_data[0]['last_updated'])
-          btc_price.append(new_data[0]['price_usd'])
-          #sets old data to the now added data so if it is received again it won't append it
-          old_data = new_data
+      if new_data != old_data:
+         btc_time.append(new_data[0]['last_updated'])
+         btc_price.append(new_data[0]['price_usd'])
+         #sets old data to the now added data so if it is received again it won't append it
+         old_data = new_data
       #calls the graph_make function to display a table every 10 info points, this is only for testing
       if len(btc_time) % 10 == 0:
-          graph_make(btc_time, btc_price)
+         graph_make(btc_time, btc_price)
       #waits 5 seconds before trying again so I don't send too many http requests
       time.sleep(5)
 
