151b3f4072874011d16c4b84b67de232f993eac2
James Woods <woods_j@MARVAL.CO.UK>
2019-04-03 14:23:12 +0100

    First commit

---
+++ bitcoin_track.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from matplotlib import pyplot
+import time
+btc_time = []
+btc_price = []
+old_data = ''
+
+def get_data():
+    btc = requests.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
+    new_data = json.loads(btc.text)
+    if new_data != old_data:
+        btc_time.append(new_data[0]['last_updated'])
+        btc_price.append(new_data[0]['price_usd'])
+    old_data = new_data
+    if len(btc_time) % 10 == 0:
+        graph_make(btc_time, btc_price)
+
+
+def graph_make(btc_time, btc_price):
+    pyplot.scatter(btc_time, btc_price)
+    pyplot.show()
+
+
+while True:
+    get_data()
+    time.sleep(5)
